a8040d28331f58545f6c810fd20f11de285de150
simasuiyuan <simasuiyuan@hotmail.com>
2022-03-17 17:51:00 +0800

    mongoDB.py

--- Project/mongoDB.py
+++ Project/mongoDB.py
@@ -7,13 +7,14 @@ password="2pZGb4axFMwpl3qR"
 client = pymongo.MongoClient(f"mongodb+srv://shared_user:{password}@bigdata.sbz3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client.test
 # %%
-# create database
-# mydb = client["CS5425"]
+# create/get database
+mydb = client["CS5425"]
 # print(client.list_database_names())
 # """MongoDB waits until you have created a collection (table), 
 # with at least one document (record) before it actually creates the database (and collection).
 # """
-# mycol = mydb["image_embedding"]
+# get collections: table
+mycol = mydb["image_embedding"]
 # image_dataset = pd.read_csv("./data/ig_data.csv").to_dict('records')
 # x = mycol.insert_many(image_dataset)
 print(client.list_database_names())
