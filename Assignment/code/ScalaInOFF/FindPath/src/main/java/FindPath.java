a214b67292d5effcf4dda3a679f95eae6e726ff8
simasuiyuan <simasuiyuan@hotmail.com>
2022-03-23 01:09:52 +0800

    update assignment with BFS solution

--- Assignment/code/ScalaInOFF/FindPath/src/main/java/FindPath.java
+++ Assignment/code/ScalaInOFF/FindPath/src/main/java/FindPath.java
@@ -23,7 +23,10 @@ import org.apache.spark.graphx.*;
 import scala.collection.JavaConverters;
 import scala.collection.mutable.WrappedArray;
 
-
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class FindPath {
@@ -42,7 +45,7 @@ public class FindPath {
         return Math.sqrt(distance);
     }
 
-    public static Dataset<Row> getNodeDF(SQLContext sqlContext, String raw_osm_file, String createTempViewName){
+    private static Dataset<Row> getNodeDF(SQLContext sqlContext, String raw_osm_file, String createTempViewName){
         Map<String, String> options = new HashMap<>();
         List<StructField> fields = new ArrayList<StructField>();
         fields.add(DataTypes.createStructField("_id", DataTypes.LongType, true));
@@ -62,7 +65,7 @@ public class FindPath {
         return nodeDF;
     }
 
-    public static Dataset<Row> getRoadDF(SQLContext sqlContext, String raw_osm_file, String createTempViewName){
+    private static Dataset<Row> getRoadDF(SQLContext sqlContext, String raw_osm_file, String createTempViewName){
         Map<String, String> options = new HashMap<>();
         List<StructField> fields = new ArrayList<StructField>(); //reset fields for ways
         fields.add(DataTypes.createStructField("_id", DataTypes.LongType, true));
@@ -91,7 +94,7 @@ public class FindPath {
         return roadDF;
     }
 
-    public static Dataset<Row> getRelationDF(SQLContext sqlContext, String nodeDFViewName, String roadDFViewName, String createTempViewName){
+    private static Dataset<Row> getRelationDF(SQLContext sqlContext, String nodeDFViewName, String roadDFViewName, String createTempViewName){
 
         Dataset<Row> expwayNodeDF = sqlContext.sql(
                 String.format("select road_id, exploded.nd_index+1 as indexedNode, node._ref as node_id, oneway " +
@@ -123,18 +126,47 @@ public class FindPath {
         return full_relation;
     }
 
+    private static String extractStringPath(SQLContext sqlContext, Dataset<Row> paths){
+        String edge_vertices = new String();
+        for(String col: paths.columns()){
+            if(col.contains("v")) edge_vertices += String.format("%s.id, ", col);
+        }
+        paths.createOrReplaceTempView("paths");
+        Row result = sqlContext.sql(
+                "select from.id, " +
+                        edge_vertices + "to.id " +
+                        "from paths"
+        ).first();
+        String res_string = new String();
+        for(int i=0; i<result.size(); i++){
+            if(i<result.size()-1){
+                res_string+=String.format("%d -> ", result.getLong(i));
+            } else {
+                res_string+=Long.toString(result.getLong(i));
+            }
+        }
+        return res_string;
+    }
+
+    private static String getPathsFromBFS(SQLContext sqlContext, GraphFrame g, String from_node, String to_node){
+        from_node = String.format("id = %s", from_node);
+        to_node = String.format("id = %s", to_node);
+        Dataset<Row> paths = g.bfs().fromExpr(from_node).toExpr(to_node).run();
+        return extractStringPath(sqlContext, paths);
+    }
+
     /* Define & register UDFS */
     private static void register_UDFs(SQLContext sqlContext){
         sqlContext.udf().register("checkOneWay", new UDF1<WrappedArray<Row>, Boolean>(){
             private static final long serialVersionUID = -5372447039252716846L;
             @Override
             public Boolean call(WrappedArray<Row> tags) throws Exception {
-               for(int i=0; i<tags.size(); i++){
-                   Row tag = tags.apply(i);
-                   if(tag.getString(0).equals("oneway") & tag.getString(1).equals("yes")){
-                       return true;
-                   }
-               }
+                for(int i=0; i<tags.size(); i++){
+                    Row tag = tags.apply(i);
+                    if(tag.getString(0).equals("oneway") & tag.getString(1).equals("yes")){
+                        return true;
+                    }
+                }
                 return false;
             }
         }, DataTypes.BooleanType);
@@ -151,7 +183,7 @@ public class FindPath {
     static boolean runOnCluster = false;
     static SparkConf sparkConf = new SparkConf().setAppName("FindPath");
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (!runOnCluster) {
             sparkConf.setMaster("local[2]");
             sparkConf.setJars(new String[] { "target/eduonix_spark-deploy.jar" });
@@ -160,35 +192,48 @@ public class FindPath {
         JavaSparkContext jsc = new JavaSparkContext(sparkConf);
         SQLContext sqlContext = new SQLContext(jsc);
         String raw_osm_file = args[0];
+        String input_src_dst_file = args[1];
+        String result_adjmap_file = args[2];
+        String result_path_file = args[3];
+
+        long start = System.currentTimeMillis();
+        long end;
+        float runtime;
+
         register_UDFs(sqlContext);
 
+        /* construct relation table */
         Dataset<Row> nodeDF = getNodeDF(sqlContext, raw_osm_file, "nodeDF");
         Dataset<Row> roadDF = getRoadDF(sqlContext, raw_osm_file, "roadDF");
         Dataset<Row> relationDF = getRelationDF(sqlContext, "nodeDF", "roadDF","relationDF");
-//        sqlContext.sql("select * from relationDF where road_id == 104661187").show(10);
         GraphFrame g = new GraphFrame(nodeDF.withColumnRenamed("node_id", "id"),
-                relationDF.withColumnRenamed("source", "src").withColumnRenamed("destination", "dst"));
-        /*BFS*/
-        String from_node = "id = 1534830314";
-        String to_node = "id = 1534822530";
-        Dataset<Row> paths = g.bfs().fromExpr(from_node).toExpr(to_node).run();
-        paths.printSchema();
+                relationDF.withColumnRenamed("source", "src").withColumnRenamed("destination", "dst")).cache();
 
-        List<String> nodes_on_path = Arrays.asList("from.id");
-        for(String col: paths.columns()){
-            if(col.contains("v")) nodes_on_path.add(String.format("%s.id", col));
+        List<String> all_results = new ArrayList<String>();
+        Scanner input_tasks = new Scanner(new FileInputStream(input_src_dst_file));
+        while(input_tasks.hasNextLine()){
+            String[] task = input_tasks.nextLine().split(" ");
+            /*BFS*/
+            System.out.println(String.format("Processing task: \nsrc node: %s -> dst node: %s", task[0], task[1]));
+            String result = getPathsFromBFS(sqlContext, g, task[0], task[1]);
+            //check run time for task
+            end = System.currentTimeMillis();
+            runtime = (end - start) / 1000F;
+            System.out.println(String.format("path found: %s", result));
+            System.out.println(String.format("Runtime: %f seconds", runtime));
+
+            all_results.add(result);
         }
-        nodes_on_path.add("to.id");
-        Dataset<Row> resPath = paths.select(JavaConverters.asScalaIteratorConverter(nodes_on_path).asScala().toSeq());
-//        paths.show();
-        List<Double> inter_edges = resPath.first().getList(0);
-        String result = String.format("%f ", inter_edges.get(0));
-        for(Double node: inter_edges){
-            result+=String.format("-> %f ", node);
-        }
-//        String str_path = "1534830314 ->";
-//        for (inter_node: )
-        System.out.println(result);
         jsc.stop();
+
+        FileWriter fw = new FileWriter(result_path_file);
+        for(String res: all_results){
+            fw.write(res+"\n");
+        }
+        fw.close();
+
+        end = System.currentTimeMillis();
+        runtime = (end - start) / 1000F;
+        System.out.println(String.format("Job done overall Runtime: %f seconds", runtime));
     }
 }
